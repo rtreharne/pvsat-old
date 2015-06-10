005313df5bfc1bfa41845ab059d1360ad157023b
Robert <R.Treharne@liverpool.ac.uk>
2015-06-10 09:12:23 +0100

    speakers tweak

--- speakers/models.py
+++ speakers/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Speaker(models.Model):
     forename = models.CharField(max_length=128, unique = True)
     surname = models.CharField(max_length=128, unique = True)
-    affiliation = models.CharField(max_length=128, unique = True)
+    affiliation = models.CharField(max_length=128)
     url = models.URLField()
     pic = models.ImageField(upload_to='logo_images', blank=True)
 
