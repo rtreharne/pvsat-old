0e8c7ccd8f65018acf8a3cb53896f0a3dd19747e
Robert <R.Treharne@liverpool.ac.uk>
2015-07-05 00:36:12 +0100

    content added: exhibitors and sponsors to programme page

--- programme/views.py
+++ programme/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from programme.models import Speaker, Theme
+from exhibitors.models import Exhibitor
+from sponsors.models import Sponsor
 
 def programme(request):
     speakers = Speaker.objects.all()
     themes = Theme.objects.all()
+    exhibitors = Exhibitor.objects.all()
+    sponsors = Sponsor.objects.all()
     programme_dict = {'speakers': speakers,
-                      'themes': themes}
+                      'themes': themes,
+                      'exhibitors': exhibitors,
+                      'sponsors': sponsors}
     return render(request, 'programme.html', programme_dict)
 
 
